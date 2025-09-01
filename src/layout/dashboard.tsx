df17d2e745ce5992007e59f0d98983ff72ddf5b6
JBartrop <jonbartplange@gmail.com>
2025-09-01 16:23:41 +0000

    sidebar done

--- src/layout/dashboard.tsx
+++ src/layout/dashboard.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Outlet } from "react-router-dom";
 import Loader from '../components/loader';
+import Sidebar from '../components/sidebar';
 
 
 
 const DashboardLayout: React.FC = () => {
     const [loading, setLoading] = useState<boolean>(true);
+    const [collapsed, isOpen] = useState<boolean>(false);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -19,9 +21,12 @@ const DashboardLayout: React.FC = () => {
 
     return(
         <section>
-            <main>
-                <h1>DashboardLayout</h1>
-                <Outlet />
+            <main className='flex min-h-screen min-w-fit'>
+                <Sidebar isOpen={collapsed} toggleOpen={() => isOpen(prev => !prev)} />
+                <div className={`${collapsed ? "ml-[3.75rem]" : "ml-64"} transition-all duration-300 p-4`}>
+                    <Outlet />
+                </div>
+                
             </main>
         </section>
     )
